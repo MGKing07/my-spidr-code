0a1979fe3357472301152b3f4269eb3174a79d9e
Mario Guerra <maguerra01@live.com>
2025-07-14 10:23:39 -0500

    Added the rest of the fields needed

--- src/components/GuessForm.js
+++ src/components/GuessForm.js
@@ -8,6 +8,26 @@ function GuessForm() {
             <label><strong>Enter First Name:</strong></label>
             <input className='TypingIn' type='text'/>
         </div>
+        <div className="FormSec">
+            <label><strong>Enter Last Name:</strong></label>
+            <input className='TypingIn' type='text'/>
+        </div>
+        <div className="FormSec">
+            <label><strong>Enter Phone #:</strong></label>
+            <input className='TypingIn' type='tel'/>
+        </div>
+        <div className="FormSec">
+            <label><strong>Enter Email:</strong></label>
+            <input className='TypingIn' type='email'/>
+        </div>
+        <div className="FormSec">
+            <label><strong>Enter Price Guess:</strong></label>
+            <input className='TypingIn' type='text'/>
+        </div>
+        <div className="FormSec">
+            <label><strong>Enter Spidr PIN:</strong></label>
+            <input className='TypingIn' type='text'/>
+        </div>
     </form>
   )
 }
