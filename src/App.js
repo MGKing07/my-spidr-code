81a48d413dabfa51a70989757f438ea59522b3af
Mario Guerra <maguerra01@live.com>
2025-07-14 10:17:12 -0500

    Decided to make form section it's own component file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import GuessForm from './components/GuessForm.js';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
         <h1>
           Guess Air Fryer Price<div className="TextL"></div>
         </h1>
+        <GuessForm/>
       </div>
     </div>
   );
