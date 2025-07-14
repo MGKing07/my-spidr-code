0787e09b1f9a1065d31a3f531e349e1799f44f47
Mario Guerra <maguerra01@live.com>
2025-07-14 11:25:56 -0500

    Added placeholders to the fields

--- src/components/GuessForm.js
+++ src/components/GuessForm.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import "../styles/GuessForm.css"
 
 function GuessForm({onSubmit, firstF, lastF, 
@@ -10,32 +9,32 @@ function GuessForm({onSubmit, firstF, lastF,
         <div className="FormSec">
             <label><strong>Enter First Name:</strong></label>
             <input required className='TypingIn' onChange={firstF} 
-            value={firstV} type='text'/>
+            value={firstV} type='text' placeholder='Jane'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Last Name:</strong></label>
             <input required className='TypingIn' onChange={lastF} 
-            value={lastV} type='text'/>
+            value={lastV} type='text' placeholder='Doe'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Phone #:</strong></label>
             <input required className='TypingIn' onChange={phoneF} 
-            value={phoneV} type='tel'/>
+            value={phoneV} type='tel' placeholder='###-###-####'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Email:</strong></label>
             <input required className='TypingIn' onChange={emailF} 
-            value={emailV} type='email'/>
+            value={emailV} type='email' placeholder='user@email.com'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Price Guess:</strong></label>
             <input required className='TypingIn' onChange={priceF} 
-            value={priceV} type='text'/>
+            value={priceV} type='text' placeholder='##.##'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Spidr PIN:</strong></label>
             <input required className='TypingIn' onChange={pinF} 
-            value={pinV} type='text'/>
+            value={pinV} type='text' placeholder='####-####-####-####'/>
         </div>
         <input className='SubmitBtn' type='submit' value="Submit"/>
     </form>
