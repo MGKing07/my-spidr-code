badd7f6635121a1317e11ac1bf00fc30420afc67
Mario Guerra <maguerra01@live.com>
2025-07-14 11:37:19 -0500

    Added patterns and maxlengths to obtain more valid inputs

--- src/components/GuessForm.js
+++ src/components/GuessForm.js
@@ -9,17 +9,19 @@ function GuessForm({onSubmit, firstF, lastF,
         <div className="FormSec">
             <label><strong>Enter First Name:</strong></label>
             <input required className='TypingIn' onChange={firstF} 
-            value={firstV} type='text' placeholder='Jane'/>
+            value={firstV} type='text' placeholder='Jane' pattern="[A-Za-z]+"/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Last Name:</strong></label>
             <input required className='TypingIn' onChange={lastF} 
-            value={lastV} type='text' placeholder='Doe'/>
+            value={lastV} type='text' placeholder='Doe' pattern="[A-Za-z]+"/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Phone #:</strong></label>
             <input required className='TypingIn' onChange={phoneF} 
-            value={phoneV} type='tel' placeholder='###-###-####'/>
+            value={phoneV} type='tel' maxLength="12" 
+            placeholder='###-###-####'
+            pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Email:</strong></label>
@@ -29,12 +31,15 @@ function GuessForm({onSubmit, firstF, lastF,
         <div className="FormSec">
             <label><strong>Enter Price Guess:</strong></label>
             <input required className='TypingIn' onChange={priceF} 
-            value={priceV} type='text' placeholder='##.##'/>
+            value={priceV} type='text' placeholder='##.##' 
+            pattern='^[0-9]*\.[0-9]{2}$'/>
         </div>
         <div className="FormSec">
             <label><strong>Enter Spidr PIN:</strong></label>
             <input required className='TypingIn' onChange={pinF} 
-            value={pinV} type='text' placeholder='####-####-####-####'/>
+            value={pinV} type='text' maxLength="19" 
+            placeholder='####-####-####-####' 
+            pattern='[0-9]{4}-[0-9]{4}-[0-9]{4}-[0-9]{4}'/>
         </div>
         <input className='SubmitBtn' type='submit' value="Submit"/>
     </form>
